62a8d62d71560ddcf4a6bf5350e3c47eb88860bc
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-01 22:16:11 -0500

    new actor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'actors/index'
-  get '/actors' => 'actors#index'
+  # get 'actors/index'
+  # get '/actors' => 'actors#index'
+  # get '/actors/new' => 'actors#new'
+  # post '/actors' => 'actors#create'
+  
+  resources :actors
+
 end
