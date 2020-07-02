b04fa662ea909f0d5c8de0146c73bd8896a42a44
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-01 23:45:53 -0500

    new actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,14 +2,11 @@ class ActorsController < ApplicationController
     def index
         @actors = Actor.all
     end
-
     def new
-        @actor = Actor.new        
+        @actor = Actor.new
     end
-
     def create
         @actor = Actor.new(actor_params)
-        puts "este es el mensaje"
         if @actor.save
             redirect_to actors_path
         else
@@ -20,7 +17,8 @@ class ActorsController < ApplicationController
 
     private
     def actor_params
+        puts params
         params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+        
     end
-
 end
